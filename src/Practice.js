1ecbbebdd04095f4ed1fb3677bdfb6d2379513c3
Brandon Prange <bprange93@gmail.com>
2021-07-26 11:53:01 -0500

    Went over let and started touching on const. Both are used only in the block as oppose to var which can be used throughout the function.

--- src/Practice.js
+++ src/Practice.js
@@ -1,5 +1,9 @@
-function sayHello(){
-    for (var i =0; i < 5; i++) {
+//using the var variable makes the variable only accessible inside of the function. 
+//using the let variable keyword makes the variable only accessible inside of the block it is being used in. 
+//Majority of the time get used to using the let keyword
+//const is the same as let being only block scoped.
+function sayHello(){    
+    for (let i =0; i < 5; i++) {
         console.log(i);
     }
 }
