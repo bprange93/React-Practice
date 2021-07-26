0a0c847425e593ec6394b33dc0897a441461af64
Brandon Prange <bprange93@gmail.com>
2021-07-26 14:50:22 -0500

    Working with components in React. Tutorial wasn't helping. Will look for new tutorial pertaining to React to keep moving forward. The tutorial had me make a file but when called it was causing errors and wouldn't load page properly till original App.js was put back in as endpoint.

---
+++ src/components/counter.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+//used code snippet to create these shortcuts. imrc for the import react component and cc for the class component
+class  Counter extends Component {
+    render() { 
+        return <h1>Hello World</h1>;
+    }
+}
+ 
+export default Counter;
